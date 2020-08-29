78b809de002d503063e346958bbf24bef3e8cb06
Vishwas Singh <singh.vishu234@gmail.com>
2020-08-29 11:49:37 +0530

    [Add]:Blog listing(4)

--- complete_list/blogs/blog.php
+++ complete_list/blogs/blog.php
@@ -26,8 +26,8 @@ if(isset($_GET['id'])){
     <link rel="stylesheet" href="blogs.css" type="text/css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700;800;900&family=Roboto:wght@700;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../OwlCarousel2-2.3.4/docs/assets/owlcarousel/assets/owl.carousel.css">
-    <link rel="stylesheet" href="../OwlCarousel2-2.3.4/docs/assets/owlcarousel/assets/owl.theme.default.css">
+    <link rel="stylesheet" href="../assets/owlcarousel/assets/owl.carousel.css">
+    <link rel="stylesheet" href="../assets/owlcarousel/assets/owl.theme.default.css">
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 </head>
 <body>
@@ -116,7 +116,7 @@ if(isset($_GET['id'])){
     </section>
 
       <script src="https://code.jquery.com/jquery-2.1.3.js"></script>
-      <script src="../OwlCarousel2-2.3.4/docs/assets/owlcarousel/owl.carousel.min.js"></script>
+      <script src="../assets/owlcarousel/owl.carousel.min.js"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
     
 
